0733f3fc264ec601b96462edde2e4d64b7b00e40
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-11 14:48:07 +0000

    MS40G-user 8.1.0 V4_20180717 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,2 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_ML-TI-MS40G.mk
---
+++ BoardConfig.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/multilaser/ML-TI-MS40G
+BOARD_VENDOR := multilaser
+
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+
+-include vendor/multilaser/ML-TI-MS40G/BoardConfigVendor.mk
